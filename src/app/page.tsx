ac2ef1dc81936e69a2d0be997786feeaab91902e
Bandeira <bandeiradev.ti@gmail.com>
2026-03-18 15:24:08 -0300

    feat: Add Design System

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,58 @@
-import Image from "next/image";
+import { Container } from "@/components/ui/container";
+import { TouristCard } from "@/components/features/tourist-card";
+import { Heading } from "@/components/ui/heading";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <>
+      <main className="min-h-screen bg-background py-10">
+        <Container>
+          <div className="mb-8 mt-10">
+            <Heading>Pontos Turísticos de Croatá</Heading>
+          </div>
+
+          <div className="grid md:grid-cols-3 gap-6">
+            <TouristCard
+              title="IGREJA MATRIZ NOSSA SENHORA DAS DORES"
+              description="O Turismo religioso é uma prática pontual em Croatá, tanto a igreja católica quanto as igrejas e templos evangélicos realizam eventos que atraem pessoas de todos os lugares."
+              image="https://croata.ce.gov.br/adm/fotos_municipio/FOTO_PONTO_67b5e8334857d_igreja-nossa-senhora-dos-dores.jpg"
+              category="Religioso"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+
+            <TouristCard
+              title="MIRANTE TOCA DA ONÇA"
+              description="Um importante ponto turístico ainda pouco explorado e conhecido pelo o homem. É um importante ponto turístico com um forte potencial para o desenvolvimento. lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate. "
+              image="https://croata.ce.gov.br/adm/fotos_municipio/FOTO_PONTO_67b5e8850e0eb_bb0ae3e8-892d-409a-9c71-f9e46b2894a5.jpg"
+              category="Natureza"
+            />
+
+            <TouristCard
+              title="ENCONTRO DOS RIOS"
+              description="Rio Piaus e Rio Inhuçu, um verdadeiro espetáculo da natureza que acontece no município de Croatá-ce. Espetáculo esse que originou o nome do distrito, sem dúvidas é um dos mais importantes e fabulosos atrativos turístico do nosso município."
+              image="https://croata.ce.gov.br/adm/fotos_municipio/FOTO_PONTO_67b5ea23d2c5a_Croata-28.jpg"
+              category="Natureza"
+            />
+            <TouristCard
+              title="CASA DE PEDRAS"
+              description="Mantém viva a memória de como viviam os escravos no século XIX. O Casarão de pedra revela em sua estrutura o sofrimento do negro africano. A casa é toda construída com paredes de pedra empilhada resistiu ao tempo. E em seu interior ainda há utensílios utilizados no período escravocrata."
+              image="https://croata.ce.gov.br/adm/fotos_municipio/FOTO_PONTO_67b5e9b8d5700_Croata-18.jpg"
+              category="Histórico"
+            />
+            <TouristCard
+              title="CACHOEIRA DA PRATA"
+              description="A Cachoeira da Prata, localizada no município de Croatá, Ceará, é um atrativo natural que encanta visitantes com sua beleza e tranquilidade. Situada entre as localidades de Barra do Sotero e o Sítio Vazante, a cachoeira é um destino popular para banhistas da região."
+              image="https://croata.ce.gov.br/adm/fotos_municipio/FOTO_PONTO_67b5eb0d8f785_cachoeira.jpg"
+              category="Natureza"
+            />
+            <TouristCard
+              title="BOSQUE DE CROATÁ"
+              description=" É uma floresta urbana de 60.000 m² localizada na sede do município. Conta com mais de 2.600 árvores de diversas espécies, mantidas por um sistema de irrigação exclusivo, que garante a preservação e a beleza do espaço. É um projeto inovador que transforma Croatá na única cidade da Serra a possuir um Central Park próprio."
+              image="https://croata.ce.gov.br/adm/fotos_municipio/FOTO_PONTO_6968f0b3cde09_BOSQ.jpg"
+              category="Natureza e Lazer"
+            />
+          </div>
+        </Container>
       </main>
-    </div>
+    </>
   );
 }
