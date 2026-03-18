6ac21859d05b5ada15f6a9d2f09652270f5cd902
Bandeira <bandeiradev.ti@gmail.com>
2026-03-18 19:24:42 -0300

    feat: Add modal do map

--- src/components/features/tourist-card.tsx
+++ src/components/features/tourist-card.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 // src/components/features/tourist-card.tsx
 
+import { useState } from "react";
 import {
   Card,
   CardImage,
@@ -9,12 +12,14 @@ import {
 } from "../ui/card";
 import { Button } from "../ui/button";
 import { Badge } from "../ui/badge";
+import { MapModal } from "../ui/map-modal";
 
 interface TouristCardProps {
   title: string;
   description: string;
   image: string;
   category?: string;
+  mapUrl: string;
 }
 
 export function TouristCard({
@@ -22,33 +27,55 @@ export function TouristCard({
   description,
   image,
   category,
+  mapUrl,
 }: TouristCardProps) {
+  const [isMapOpen, setIsMapOpen] = useState(false);
+
   return (
-    <Card>
-      <div className="relative">
-        <CardImage src={image} alt={title} />
+    <>
+      <Card>
+        <div className="relative">
+          <CardImage src={image} alt={title} />
 
-        {category && (
-          <div className="absolute top-3 left-3">
-            <Badge variant="accent">{category}</Badge>
+          {category && (
+            <div className="absolute top-3 left-3">
+              <Badge variant="accent">{category}</Badge>
+            </div>
+          )}
+        </div>
+
+        <CardContent>
+          <CardTitle>{title}</CardTitle>
+
+          <CardDescription>{description}</CardDescription>
+
+          <div className="flex flex-row gap-2">
+            <Button
+              className="w-full"
+              size="sm"
+              variant="primary"
+              href="https://croata.ce.gov.br/pontoturistico.php"
+            >
+              Ver mais
+            </Button>
+            <Button
+              className="w-full"
+              size="sm"
+              variant="outline"
+              onClick={() => setIsMapOpen(true)}
+            >
+              Ver no mapa
+            </Button>
           </div>
-        )}
-      </div>
+        </CardContent>
+      </Card>
 
-      <CardContent>
-        <CardTitle>{title}</CardTitle>
-
-        <CardDescription>{description}</CardDescription>
-
-        <Button
-          className="w-full"
-          size="sm"
-          variant="primary"
-          href="https://croata.ce.gov.br/pontoturistico.php"
-        >
-          Ver mais
-        </Button>
-      </CardContent>
-    </Card>
+      <MapModal
+        mapUrl={mapUrl}
+        title={title}
+        isOpen={isMapOpen}
+        onClose={() => setIsMapOpen(false)}
+      />
+    </>
   );
 }
